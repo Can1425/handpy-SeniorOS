4bbfa88e508a685bbb50621bcec6887eecf9a986
欧阳泓骞 <ouyanghongqian@qq.com>
2024-04-12 19:24:31 +0800

    feat/docs:完善update.py功能 修改文档 1. 完善「update.py」   /core/update.py func(DiffDict) 新增功能：对比字典内容   /core/update.py func(DiffUpdList) 完善功能：比对本地文件清单与更新清单   /core/update.py func(FastHash) 新增功能：以 低RAM且快速地 计算哈希值   /core/update.py func(UpdateST) 新增示例：更新系统 2. 完善「关于 update.py .md」   新增清单样式中的目录字段   完善有关「update.py」开放可调用对象的部分

--- code/SeniorOS/system/update.py
+++ code/SeniorOS/system/update.py
@@ -1,16 +1,24 @@
-import uhashlib,os,urequests
+import uhashlib,os,requests
 
 # lambda: 值查找键
 valueFindKey=lambda dictObj,value:{v : k for k, v in dictObj.items()}[value]
 
-SERVER_URL="https://server.ip/port/Flag_OS_UPD/"
+SERVER_URL="https://server.ip:port/Flag_OS_UPD/"
 SYSTEM_VERSION=eval("[/Const('version')/]")
 # 读取本地文件清单
 def GetFileList():
     with open("/SeniorOS/data/fileList.json",'rb')as f:
         return f.read().decode()
+# 快速hash By CodeGeeX
+def FastHash(file_path, hash_type=uhashlib.md5):
+    hash_obj = hash_type()
+    with open(file_path, 'rb') as f:
+        for chunk in iter(lambda: f.read(4096), b""):
+            hash_obj.update(chunk)
+    return hash_obj.hexdigest()
+
 # 获取更新清单
-def GetUpdList():
+def GetUpdList(updVersion):
     global SERVER_URL,SYSTEM_VERSION
     try:
         r=requests.get("{url}/GetUpdList/{sysversion}/{updversion}".format(SERVER_URL,SYSTEM_VERSION,updVersion))
@@ -20,10 +28,10 @@ def GetUpdList():
     assert r.status_code == 200 ,"错误的HTTP状态码"
     return r.json()
 # 获取最新版本
-def GetLatestVer(version):
+def GetLatestVer():
     global SERVER_URL
     try:
-        r=requests.get("{}/LatestVer/version".format(SERVER_URL,version))
+        r=requests.get("{}/LatestVer/version".format(SERVER_URL))
     except OSError as e:
         if e.args[0]==113:
             raise AssertionError("连接超时/路由不可达")
@@ -44,28 +52,59 @@ def UpdFile(updList):
     for i in updList:       # 这里的updList对应GetUpdList的返回值的["updateList"]
         with open(i["filePath"],'wb') as f:
             f.write(GetUpdFile(i["fileMd5"]))
-# 获取本地文件MD5
-def GetFileMD5(fileName):
-    with open(fileName,'rb') as f:
-        data = f.read()
-        h = uhashlib.md5(data)
-        return h
 # 根据对应MD5下载配置文件更新策略并运行    
 def UpdCfg(updCfgST_MD5):
     with open("/cfgUpdST.py",'rb')as f:
         f.write(GetUpdFile(updCfgST_MD5))
     os.chdir("/");__import__('cfgUpdST').UpdateST()
-# 对比更新清单 返回需要更新的部分
+# 对比更新清单 返回需要更新的部分 By CGPT
+# https://chat.openai.com/share/f59bd4e3-ff2f-426a-b13d-4f2af52502b3
+def DiffDict(oldData, newData):
+    oldKeys = set(oldData.keys())
+    newKeys = set(newData.keys())
+    added_or_modified = {key: newData[key] for key in newKeys if key not in oldKeys or newData[key] != oldData[key]}
+    removed = {key: oldData[key] for key in oldKeys if key not in newKeys}
+    return added_or_modified, removed
 def DiffUpdList(fileList:dict,updList:dict)->dict:
-    updList_True={}
-    for name,md5 in enumerate(...):
-        ... # TODO:完成此处对比模块
-            
+    needUpdData,needRmData=DiffDict(fileList,updList)
+    updFile=[]
+    updDir=[]
+    rmFile=[]
+    rmDir=[]
+    for k, v in needUpdData.items():
+        if k == "dir":
+            for i in v:updDir.append(i)
+        else:
+            updFile.append({k:v})
+    for k, v in needRmData.items():
+        if k == "dir":
+            for i in v:rmDir.append(i)
+        else:
+            rmFile.append({k:v})
 
+    return updFile,updDir,rmFile,rmDir
 
-""" # 因实际需要 会插入一定的UI展示 所以仅作一个例子
-def Update():
+# By CodeGeeX
+def UpdateST():
+    global SERVER_URL,SYSTEM_VERSION
+    # 获取本地文件清单
+    fileList=GetFileList()
+    # 获取更新清单
     updList=GetUpdList()
-    UpdFile(updList)
+    # 对比更新清单 返回需要更新的部分
+    updFile,updDir,rmFile,rmDir=DiffUpdList(eval(fileList),updList)
+    # 下载并获取文件
+    UpdFile(updFile)
+    # 更新文件夹
+    for i in updDir:
+        try:os.mkdir(i)
+        except:pass
+    # 删除文件
+    for i in rmFile:
+        os.remove(i["filePath"])
+    # 删除文件夹
+    for i in rmDir:
+        try:os.rmdir(i)
+        except:pass
+    # 更新配置文件
     UpdCfg(updList["cfgUpdST"])
-"""
--- docs/关于 update.py .md
+++ docs/关于 update.py .md
@@ -19,8 +19,15 @@
     // 更新清单
     "updateList": {
         "update.py": "698d51a1wdnmd21ce581499d7b701668f",
-        "/dir/update.md":"698d51191981021ce58149cjbgj01668f"
+        "/dir/update.md":"698d51191981021ce58149cjbgj01668f",
         // 剩下的同理.
+
+        "dir":[
+            "dir1",
+            "dir2",
+            "dir1/dir3"
+            // 剩下的同理.
+        ]
     },
     // 配置文件更新策略MD5
     "updCfgST":"698d51a19d8a12111451499d7b701668f" //也是MD5
@@ -30,3 +37,40 @@
 此为单次升级的升级清单
 
 一般情况下 清单的URL为`{IP}/GetUpdList/{currentVer}/{updateVer}`
+
+# 关于各类对象
+
+| 函数名 | 类型 | 功能 | 参数 | 返回值 | 备注 |
+|-|-|-|-|-|-|
+|FastHash|func|快速/低ram的hash文件|文件路径,\[可选\]哈希类型|哈希值|/|
+|GetFileList|func|获取本地文件清单|/|本地文件清单数据|/|
+| GetUpdList | func | 获取升级清单 | / | 升级清单 | 返回升级清单 | / |
+| GetLatestVer | func | 获取最新版本的版本号 | / | 版本号 | / | / |
+| GetUpdFile | func | 获取升级文件 | 文件MD5 | 文件内容 | / | /|
+| UpdFile | func | 覆写需要更新的文件 | 预处理好的更新清单 | / | /|/|
+| UpdCfg | func | 下载并运行配置升级程序 | 配置升级程序Hash值 | /| /|
+| Update| func | 一键式更新系统| / | / | /|
+
+# 注意
+
+- 升级清单的URL为`{IP}/GetUpdList/{currentVer}/{updateVer}`
+- 升级清单的URL为`{IP}/GetUpdFile/{fileMD5}`
+
+# 关于自定义化更新
+
+我们将各类更新步骤拆分开来 便于各位开发者自定义化
+## 步骤
+- **获取** 本地文件清单 与 更新清单
+- **对比** 更新清单
+- **写入** 需要更新的文件/文件夹
+- **删除** 不需要的文件/文件夹
+- **运行** 配置升级程序
+- **重启** 重启SeniorOS
+
+>我们编写了一个示例 位于`core/update.py func(UpdateST)`
+>
+>各位开发者可以参照此示例进行编写 插入所需的UI/交互等
+# 注意
+
+- 升级清单的URL为`{IP}/GetUpdList/{currentVer}/{updateVer}`
+- 升级清单的URL为`{IP}/GetUpdFile/{fileMD5}`
