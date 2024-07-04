7a0f9168805a5dc2feed232e2327b8ff30f4f926
CycleBai <unbounded.sydney@gmail.com>
2024-07-04 17:16:53 +1000

    [fix] Fix Python Version Error Problem.
    为 1eab3f496f8750f5967d4bbabb0cbbee30fec4ca 提交修复错误。

--- initBuildEnv.bat
+++ initBuildEnv.bat
@@ -3,32 +3,41 @@ echo Senior OS Build Runtime Quick Initialization Tool
 echo    - By @Mask:
 echo    - Released Under MIT license.
 
-:VENV
+:CREATE_VENV
 set /p choice="Do you want to create Python Virtual Environments? (Y/n): "
 if /i "%choice%"=="y" (
     echo OK
-    python -m venv .venv
+    :PYTHON_VERSION
+    set /p py_version="Are you using Python 2 or Python 3? (2/3): "
+    if "%py_version%"=="2" (
+        python -m venv .venv
+    ) else if "%py_version%"=="3" (
+        python3 -m venv .venv
+    ) else (
+        echo Invalid Input, please enter 2 or 3.
+        goto PYTHON_VERSION
+    )
     call .\.venv\Scripts\activate.bat
 ) else if /i "%choice%"=="n" (
     echo OK
 ) else (
     echo Invalid Input, please enter Y or N.
-    goto VENV
+    goto CREATE_VENV
 )
 
 echo Python Virtual Environments create finished or canceled, start install Python library...
 
-:PIP
-set /p choice="Are you using Pip 2 or Pip 3? (2/3): "
-if "%choice%"=="2" (
+:INSTALL_LIB
+set /p pip_version="Are you using Pip 2 or Pip 3? (2/3): "
+if "%pip_version%"=="2" (
     pip install mpy-cross-v5
     pip install GitPython
-) else if "%choice%"=="3" (
+) else if "%pip_version%"=="3" (
     pip3 install mpy-cross-v5
     pip3 install GitPython
 ) else (
     echo Invalid Input, please enter 2 or 3.
-    goto PIP
+    goto INSTALL_LIB
 )
 
 echo [INFO] Successful initialization SeniorOS Build Environment.
