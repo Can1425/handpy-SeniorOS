a924bd1cb529df11d1b2db94da10f0096d13c695
Gxxk <gxxk@duck.com>
2024-07-29 15:43:16 +0800

    refactor:重构App的LOGO存储方式 1. code/SeniorOS/apps/port.py 适配logo.py的字典存储方式 2. code/SeniorOS/apps/logo.py 修改其存储方式为字典 3. 小修小补 优化性能/代码排版

--- code/SeniorOS/apps/port.py
+++ code/SeniorOS/apps/port.py
@@ -1,6 +1,6 @@
 from mpython import *
 import time
-import SeniorOS.apps.logo as logo
+from SeniorOS.apps.logo import Logo
 import SeniorOS.system.daylight as DayLight
 import SeniorOS.system.core as Core
 import sys
@@ -11,9 +11,9 @@ List = Core.Data.Get("list", "localAppName")
 
 def AppDynamic():
     global waitTime, select1X, select2X, select3X, select4X, appNum, operationalJudgment
-    if touchpad_p.is_pressed() and touchpad_y.is_pressed():
+    if eval("[/GetButtonExpr('py')/]"):
         operationalJudgment = 0
-        appNum = appNum + 1
+        appNum += 1
         if appNum + 1 > len(List):
             appNum = len(List) - 1
             return
@@ -23,26 +23,23 @@ def AppDynamic():
         select3X = 31
         select4X = 85
         for count in range(4):
-            select1X = select1X + 18
-            select2X = select2X + 18
-            select3X = select3X + 18
-            select4X = select4X + 18
+            select1X += 18
+            select2X += 18
+            select3X += 18
+            select4X += 18
             oled.fill_rect(0, 0, 128, 43, 0)
+            try:oled.Bitmap(select1X, 10, Logo[appNum+1], 25, 25, 1)
+            except:pass
+            oled.Bitmap(select1X, 10, Logo[appNum], 25, 25, 1)
             try:
-                exec("oled.Bitmap(select1X, 10, logo.App" + str(appNum + 1) + ", 25, 25, 1)")
-            except:
-                pass
-            exec("oled.Bitmap(select2X, 10, logo.App" + str(appNum) + ", 25, 25, 1)")
-            try:
-                exec("oled.Bitmap(select3X, 10, logo.App" + str(appNum - 1) + ", 25, 25, 1)")
-                exec("oled.Bitmap(select4X, 10, logo.App" + str(appNum - 2) + ", 25, 25, 1)")
-            except:
-                pass
+                oled.Bitmap(select1X, 10, Logo[appNum-1], 25, 25, 1)
+                oled.Bitmap(select1X, 10, Logo[appNum-2], 25, 25, 1)
+            except:pass
             oled.fill_rect(0, 47, 128, 18, 0)
             oled.show()
             time.sleep_ms(waitTime)
-            waitTime = waitTime + 20
-    if touchpad_o.is_pressed() and touchpad_n.is_pressed():
+            waitTime += 20
+    if eval("[/GetButtonExpr('on')/]"):
         operationalJudgment = 1
         appNum = appNum - 1
         if appNum < 0:
@@ -54,25 +51,22 @@ def AppDynamic():
         select3X = 64
         select4X = 10
         for count in range(4):
-            select1X = select1X + -18
-            select2X = select2X + -18
-            select3X = select3X + -18
-            select4X = select4X + -18
+            select1X -= 18
+            select2X -= 18
+            select3X -= 18
+            select4X -= 18
             oled.fill_rect(0, 0, 128, 43, 0)
+            try:oled.Bitmap(select1X, 10, Logo[appNum-1], 25, 25, 1)
+            except:pass
+            oled.Bitmap(select1X, 10, Logo[appNum], 25, 25, 1)
             try:
-                exec("oled.Bitmap(select1X, 10, logo.App" + str(appNum - 1) + ", 25, 25, 1)")
-            except:
-                pass
-            exec("oled.Bitmap(select2X, 10, logo.App" + str(appNum) + ", 25, 25, 1)")
-            try:
-                exec("oled.Bitmap(select3X, 10, logo.App" + str(appNum + 1) + ", 25, 25, 1)")
-                exec("oled.Bitmap(select4X, 10, logo.App" + str(appNum + 2) + ", 25, 25, 1)")
-            except:
-                pass
+                oled.Bitmap(select1X, 10, Logo[appNum+1], 25, 25, 1)
+                oled.Bitmap(select1X, 10, Logo[appNum+2], 25, 25, 1)
+            except:pass
             oled.fill_rect(0, 47, 128, 18, 0)
             oled.show()
             time.sleep_ms(waitTime)
-            waitTime = waitTime + 20
+            waitTime += 20
 
 def App():
     global waitTime, select1X, select2X, select3X, select4X, appNum
@@ -80,16 +74,16 @@ def App():
     select1X = -5
     select2X = 49
     oled.fill(0)
-    oled.Bitmap(select1X, 10, logo.App1, 25, 25, 1)
-    oled.Bitmap(select2X, 10, logo.App0, 25, 25, 1)
+    oled.Bitmap(select1X, 10, Logo[1], 25, 25, 1)
+    oled.Bitmap(select2X, 10, Logo[0], 25, 25, 1)
     oled.show()
-    while not (eval("[/GetButtonExpr('a')/]") or touchpad_t.is_pressed() and touchpad_h.is_pressed()):
+    while not eval("[/GetButtonExpr('ath')/]"):
         oled.hline(4, 46, 126, 1)
         oled.show()
-        oled.DispChar(str('〔'), 30, 20, 1)
-        oled.DispChar(str('〕'), 85, 20, 1)
-        oled.DispChar(str('>'), 120, 48, 1)
-        oled.DispChar(str('<'), 1, 48, 1)
+        oled.DispChar('〔', 30, 20, 1)
+        oled.DispChar('〕', 85, 20, 1)
+        oled.DispChar('>', 120, 48, 1)
+        oled.DispChar('<', 1, 48, 1)
         oled.DispChar(List[appNum],DayLight.AutoCenter(List[appNum]), 48, 1)
         oled.show()
         AppDynamic()
@@ -101,26 +95,20 @@ def App():
             # exec(str("App"+ str(appNum) +".main()"))
             DayLight.VastSea.SeniorMove.Text(List[appNum], 2, 0, DayLight.AutoCenter(List[appNum]) + DayLight.AutoCenter(List[appNum])//2, -134)
             if operationalJudgment == 0:
+                try:oled.Bitmap(select1X, 10, Logo[appNum+1], 25, 25, 1)
+                except:pass
+                oled.Bitmap(select1X, 10, Logo[appNum], 25, 25, 1)
                 try:
-                    exec("oled.Bitmap(select1X, 10, logo.App" + str(appNum + 1) + ", 25, 25, 1)")
-                except:
-                    pass
-                exec("oled.Bitmap(select2X, 10, logo.App" + str(appNum) + ", 25, 25, 1)")
-                try:
-                    exec("oled.Bitmap(select3X, 10, logo.App" + str(appNum - 1) + ", 25, 25, 1)")
-                    exec("oled.Bitmap(select4X, 10, logo.App" + str(appNum - 2) + ", 25, 25, 1)")
-                except:
-                    pass
+                    oled.Bitmap(select1X, 10, Logo[appNum-1], 25, 25, 1)
+                    oled.Bitmap(select1X, 10, Logo[appNum-2], 25, 25, 1)
+                except:pass
                 oled.show()
             elif operationalJudgment == 1:
+                try:oled.Bitmap(select1X, 10, Logo[appNum-1], 25, 25, 1)
+                except:pass
+                oled.Bitmap(select1X, 10, Logo[appNum], 25, 25, 1)
                 try:
-                    exec("oled.Bitmap(select1X, 10, logo.App" + str(appNum - 1) + ", 25, 25, 1)")
-                except:
-                    pass
-                exec("oled.Bitmap(select2X, 10, logo.App" + str(appNum) + ", 25, 25, 1)")
-                try:
-                    exec("oled.Bitmap(select3X, 10, logo.App" + str(appNum + 1) + ", 25, 25, 1)")
-                    exec("oled.Bitmap(select4X, 10, logo.App" + str(appNum + 2) + ", 25, 25, 1)")
-                except:
-                    pass
+                    oled.Bitmap(select1X, 10, Logo[appNum+1], 25, 25, 1)
+                    oled.Bitmap(select1X, 10, Logo[appNum+2], 25, 25, 1)
+                except:pass
                 oled.show()
