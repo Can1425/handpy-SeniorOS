b470c1682f27ad6b4444e49d67d848d217e8ed93
LPLPLPLPLPLPLP <d110101010101@outlook.com>
2024-07-31 16:19:41 +0800

    feat:在原FileViewer的基础上添加了TextReader 如题，TextReader提供了入口: 使用方法 (1.)导入TextReader: import SeniorOS.apps.app2 (2.)初始化TextReader: 1-直接添加文本--- `reading=app2.Textreader("114514 \n 1919810 \n")  reading.main()` 2-直接添加文本,但是切换换行符--- `reading=app2.Textreader("114514;1919810;",";")  reading.main()` 3-添加文件--- `with open("test.txt","r") as f:      reading=app2.Textreader(f.read(),"\n")      reading.main()` --------- 操作说明: py-on翻页 A退出

--- code/SeniorOS/apps/app2.py
+++ code/SeniorOS/apps/app2.py
@@ -1,35 +1,52 @@
-path='/'
-while not eval("[/GetButtonExpr('a')/]")():
-    fileviewer(path)
-
-def IsFile(f):
-    print(f)
-    if os.stat(f)[0]<20000:return "目录"
-    else:
-        return fileattribute(f)
-def fileattribute(f):
-    last4=f[-4:]
-    last3=f[-3:]
-    if last3==".py":
-        return "可执行文件"
-    elif last4==".pbm" or last4==".bmp":
-        return "图片"
-    else:
-        return "文件"
-def waitkey(**kwargs):
-    while True:
-        for value,key in kwargs.items():
-            if key.is_pressed():
-                return value
-pathpic=bytearray([0X00,0X00,0X3E,0X00,
-                0X41,0X00,0X80,0X80,
-                0X81,0XFC,0XFE,0X02,
-                0X80,0X01,0X80,0X01,
-                0XFF,0XFF,0X80,0X01,
-                0X80,0X01,0X80,0X01,
-                0X80,0X01,0X80,0X01,
-                0X40,0X02,0X3F,0XFC,])
-filepic=bytearray([0X00,0X00,0X3F,0XC0,
+from mpython import*
+import os
+import gc
+class Textreader:
+    def __init__(self, text, splitCfg="\n"):
+        self.text = text.split(splitCfg)
+    def showText(self,lines=0):
+        for i in [0,1,2]:
+            try:
+                oled.DispChar(self.text[i+lines],0,i*16)
+            except:
+                oled.DispChar(" ",0,i*16)
+    def text_info(self,info):
+        if info == "len":
+            return int(len(self.text)/3)
+        elif info == "len3":
+            return len(self.text)%3
+    def main(self):
+        page_num=self.text_info("len")+self.text_info("len3")
+        n=0
+        while not button_a.is_pressed():
+            gc.collect()
+            oled.fill(0)
+            self.showText(n)
+            oled.DispChar("<PY",0,48);oled.DispChar("ON>",104,48)
+            oled.DispChar("{}/{}".format(int(n/3),int(page_num/3)),52,48)
+            oled.hline(0,48,128,1)
+            oled.show()
+            if touchpad_p.is_pressed() or touchpad_y.is_pressed():
+                if n-3<0:
+                    n=0
+                else:
+                    n-=3
+            elif touchpad_n.is_pressed() or touchpad_o.is_pressed():
+                if n+3>page_num:
+                    n=page_num#-self.text_info("len3")
+                else:
+                    n+=3
+class FileViewer:
+    def __init__(self):
+        self.pathpic=bytearray([0X00,0X00,0X3E,0X00,
+                            0X41,0X00,0X80,0X80,
+                            0X81,0XFC,0XFE,0X02,
+                            0X80,0X01,0X80,0X01,
+                            0XFF,0XFF,0X80,0X01,
+                            0X80,0X01,0X80,0X01,
+                            0X80,0X01,0X80,0X01,
+                            0X40,0X02,0X3F,0XFC,])
+        self.filepic=bytearray([0X00,0X00,0X3F,0XC0,
                 0X20,0X60,0X2F,0X50,
                 0X20,0X48,0X2F,0X7C,
                 0X20,0X04,0X23,0XC4,
@@ -37,7 +54,7 @@ filepic=bytearray([0X00,0X00,0X3F,0XC0,
                 0X22,0X04,0X22,0X04,
                 0X22,0X04,0X20,0X04,
                 0X3F,0XFC,0X00,0X00,])
-runpic=bytearray([0X00,0X00,0X3F,0XC0,
+        self.runpic=bytearray([0X00,0X00,0X3F,0XC0,
                 0X20,0X60,0X2F,0X50,
                 0X20,0X48,0X2F,0X7C,
                 0X20,0X04,0X23,0XC4,
@@ -45,7 +62,7 @@ runpic=bytearray([0X00,0X00,0X3F,0XC0,
                 0X22,0X04,0X22,0X04,
                 0X22,0X04,0X20,0X04,
                 0X3F,0XFC,0X00,0X00,])
-picpic=bytearray([0XFF,0XFF,0X80,0X01,
+        self.picpic=bytearray([0XFF,0XFF,0X80,0X01,
                 0X8C,0X01,0X92,0X01,
                 0X92,0X09,0X8C,0X15,
                 0X80,0X23,0X84,0X41,
@@ -53,87 +70,106 @@ picpic=bytearray([0XFF,0XFF,0X80,0X01,
                 0XA0,0X01,0XC0,0X01,
                 0X80,0X01,0X80,0X01,
                 0X80,0X01,0XFF,0XFF,])
+    def IsFile(self,f):
+        print(f)
+        if os.stat(f)[0]<20000:return "目录"
+        else:
+            return self.fileattribute(f)
+    def fileattribute(self,f):
+        last4=f[-4:]
+        last3=f[-3:]
+        if last3==".py":
+            return "可执行文件"
+        elif last4==".pbm" or last4==".bmp":
+            return "图片"
+        else:
+            return "文件"
 
-def fileviewer(initpath:str):
-    path=initpath
-    if path[:2]=="//":
-        path=path[1:]
-    Pathpic=pathpic
-    Filepic=filepic
-    Runpic=runpic
-    Picpic=picpic
-    def lastpath(path:str):
-        return "/".join(path.split("/")[:-1])
-    while True:
-        Dir=os.listdir(path)
-        DIRlen=len(Dir)
-        num=0
+    def fileviewer(self,initpath:str):
+        path=initpath
+        if path[:2]=="//":
+            path=path[1:]
+        Pathpic=self.pathpic
+        Filepic=self.filepic
+        Runpic=self.runpic
+        Picpic=self.picpic
+        def lastpath(path:str):
+            return "/".join(path.split("/")[:-1])
         while True:
-            t=IsFile("{}/{}".format(path,Dir[num]))
-            oled.fill(0)
-            oled.DispChar(Dir[num],16,0)
-            oled.DispChar("属性:{}".format(t),0,16)
-            oled.DispChar("<P           {}/{}           N>".format(num+1,DIRlen),1,48)
-            if t=="目录":
-                oled.Bitmap(0, 0,Pathpic, 16, 16, 1)
-            elif t=="文件":
-                oled.Bitmap(0,0,Filepic,16,16,1)
-            elif t=="可执行文件":
-                oled.Bitmap(0,0,Runpic,16,16,1)
-            elif t=="图片":
-                oled.Bitmap(0,0,Picpic,16,16,1)
-            oled.hline(0,16,130,1)
-            oled.show()
-            key=waitkey(A=button_a,B=button_b,Y=touchpad_y,T=touchpad_t,H=touchpad_h,O=touchpad_o,N=touchpad_n,P=touchpad_p)
-            if key=="T" or key=="H":
-                if path=="/":
-                    #print("已经是根目录")
-                    continue
-                path=lastpath(path)
-                #print("返回上一级目录:{}".format(path))
-                break
-            elif key=="B":
+            gc.collect()
+            Dir=os.listdir(path)
+            DIRlen=len(Dir)
+            num=0
+            while not button_a.is_pressed():
+                t=self.IsFile("{}/{}".format(path,Dir[num]))
+                oled.fill(0)
+                oled.DispChar(Dir[num],16,0)
+                oled.DispChar("属性:{}".format(t),0,16)
+                oled.DispChar("B)默认打开 O)文本打开",0,32)
+                oled.DispChar("<P           {}/{}           N>".format(num+1,DIRlen),1,48)
                 if t=="目录":
-                    #print("进入目录:{}".format(Dir[num]))
-                    #print("正在加载......可能需要几秒钟")
-                    #print("Path: {}/{}".format(path,Dir[num]))
-                    path="{}/{}".format(path,Dir[num])
-                    break
+                    oled.Bitmap(0, 0,Pathpic, 16, 16, 1)
                 elif t=="文件":
-                    oled.fill(0)
-                    oled.DispChar("暂时不支持文件预览",0,0)
-                    oled.DispChar("1秒后返回",0,16)
-                    oled.show()
-                    time.sleep(1)
-                    break
+                    oled.Bitmap(0,0,Filepic,16,16,1)
                 elif t=="可执行文件":
-                    libname=("{}/{}".format(path,Dir[num]))[:-3].replace("/",".")
-                    while libname[0]==".":
-                        libname=libname[1:]
-                    try:
-                        __import__(libname)
-                    except Exception as e:
-                        print("导入失败，错误信息如下：\n",e.__class__.__name__,e)
-                    else:
-                        #print("导入成功")
-                        pass
+                    oled.Bitmap(0,0,Runpic,16,16,1)
                 elif t=="图片":
-                    oled.fill(0)
-                    image_picture = Image()
-                    oled.blit(image_picture.load('{}/{}'.format(path,Dir[num])), 0, 0)
-                    oled.show()
-                    while not key=="T" or key=="H":
-                        pass
-            elif key=="N":
-                num+=1
-                if num>=DIRlen:num=0
-            elif key=="P":
-                num-=1
-                if num<0:num=DIRlen-1
+                    oled.Bitmap(0,0,Picpic,16,16,1)
+                oled.hline(0,16,130,1)
+                oled.show()
+                if touchpad_t.is_pressed() or touchpad_h.is_pressed():
+                    if path=="/":
+                        #print("已经是根目录")
+                        continue
+                    path=lastpath(path)
+                    #print("返回上一级目录:{}".format(path))
+                    break
+                if touchpad_o.is_pressed():
+                        with open("{}/{}".format(path,Dir[num]),"r") as f:
+                            readFile=Textreader(f.read())
+                            readFile.main()
+                elif button_b.is_pressed():
+                    if t=="目录":
+                        #print("进入目录:{}".format(Dir[num]))
+                        #print("正在加载......可能需要几秒钟")
+                        #print("Path: {}/{}".format(path,Dir[num]))
+                        path="{}/{}".format(path,Dir[num])
+                        break
+                    elif t=="文件":
+                        with open("{}/{}".format(path,Dir[num]),"r") as f:
+                            readFile=Textreader(f.read())
+                            readFile.main()
+                    elif t=="可执行文件":
+                        libname=("{}/{}".format(path,Dir[num]))[:-3].replace("/",".")
+                        while libname[0]==".":
+                            libname=libname[1:]
+                        try:
+                            __import__(libname)
+                        except Exception as e:
+                            print("导入失败，错误信息如下：\n",e.__class__.__name__,e)
+                        else:
+                            #print("导入成功")
+                            pass
+                    elif t=="图片":
+                        oled.fill(0)
+                        image_picture = Image()
+                        oled.blit(image_picture.load('{}/{}'.format(path,Dir[num])), 0, 0)
+                        oled.show()
+                        while not (touchpad_t.is_pressed() or touchpad_h.is_pressed()):
+                            pass
+                if touchpad_n.is_pressed():
+                    num+=1
+                    if num>=DIRlen:num=0
+                elif touchpad_p.is_pressed():
+                    num-=1
+                    if num<0:num=DIRlen-1
     #鸣谢名单
     #程序设计---LP_OVER/emofalling
     #界面设计---LP_OVER
     #bug修复/优化---emofalling
     #运行平台提供---W-Can1425
-    #感谢各位的付出!
-    #fileviewer  -by LP_OVER/emofalling
+
+path='/'
+while not button_a.is_pressed():
+    fileview=FileViewer()
+    fileview.fileviewer(path)
