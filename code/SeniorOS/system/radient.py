d0c9bb4cd95c35b7bb3bd822083fc95f5d00fc33
LP_OVER <d110101010101@outlook.com>
2024-08-30 20:59:16 +0800

    comfix:草了,写错了 写错许可证信息了()

--- code/SeniorOS/system/radient.py
+++ code/SeniorOS/system/radient.py
@@ -1,7 +1,7 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
-# SmartWifi - by LP_OVER
+# radient.py - by LP_OVER
 import socket
 class CodeError(Exception):
     pass
