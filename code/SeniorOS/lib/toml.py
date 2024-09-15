845dd23914a833c77cd50f52f1500a6f9f69aea7
LP_OVER <d110101010101@outlook.com>
2024-09-15 23:56:37 +0800

    fix:toml 极致 压缩

--- code/SeniorOS/lib/toml.py
+++ code/SeniorOS/lib/toml.py
@@ -1,30 +1,14 @@
 class toml:
-    def toml2dict(self, file):
-        js = {}
-        text = file.read()
-        if '\r\n' in text:text=text.split('\r\n')
-        else:text=text.split('\n')
+    def toml2dict(self,file):
+        js = {};text=(file.read()).split('\r\n' if '\r\n' in file.read() else '\n')
         for i in text:
             if i.startswith('#') or "=" not in i:continue
-            if i.startswith('['):
-                js[i[1:-1]] = {}
+            if i.startswith('['):js[i[1:-1]] = {}
             else:
                 string=(i.split('=')[1]).split("#")[0].strip().strip("\"").strip("\'")
-                try:js[i.split('=')[0].strip()] = eval(string,{})
-                except:js[i.split('=')[0].strip()] = string
+                js[i.split('=')[0].strip()] = eval("string")
         return js
-    def dict2toml(self, js):
+    def dict2toml(self,js):
         text = ""
-        for i in js:
-            if type(js[i]) == dict:
-                text += f"[{i}]\n"
-            if type(js[i]) == list:
-                text += f"{i} = ["
-                for j in range(len(js[i])):
-                    if j != len(js[i])-1:text += f"{js[i][j]},"
-                    else:text += f"{js[i][j]}"
-                text += "]\n"
-            else:
-                if type(js[i]) == str:text += f"{i} = \"{js[i]}\"\n"
-                else:text += f"{i} = {js[i]}\n"
+        for i in js:text += "{0} = {1}{2}{1}\n".format(i, "\"" if type(js[i]) == str else "",js[i])
         return text
