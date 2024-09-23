1ea87d5b186a2c4760070cedfa09e794181f95af
LP_OVER <d110101010101@outlook.com>
2024-09-23 20:15:21 +0800

    feat:复活ShutDown 如题

--- code/SeniorOS/style/home.py
+++ code/SeniorOS/style/home.py
@@ -12,7 +12,7 @@ import SeniorOS.lib.pages_manager as PagesManager
 import SeniorOS.system.core as Core
 
 pagesNum = 0
-DLTIME=DayLight.UITime(True)
+DLTIME=lambda:DayLight.UITime(True)
 
 #-----------------------------------------------------------------------------------#
 def Style1():
@@ -30,33 +30,35 @@ def Style2():
 class Style3Dependencies:
     def IsPYTouth():
         global pagesNum
-        if pagesNum > 0: pagesNum -= 1
-        PagesManager.Main.Import("SeniorOS.style.home", "Page" + str(pagesNum))
-        return
+        if pagesNum >= 0: pagesNum -= 1
+        PagesManager.Main.Import("SeniorOS.style.home", "Page%d"%pagesNum)
 
     def IsONTouth():
         global pagesNum
         if pagesNum <= 1: pagesNum += 1
-        PagesManager.Main.Import("SeniorOS.style.home", "Page" + str(pagesNum))
-        return
+        PagesManager.Main.Import("SeniorOS.style.home", "Page%d"%pagesNum)
     
     def Page0():
-        oled.fill(0)
-        oled.DispChar_font(SeniorOS.fonts.misans, DLTIME, 5, 10)
-        oled.DispChar_font(SeniorOS.fonts.misans_16, (''.join([str(x) for x in [time.localtime()[1], '/', time.localtime()[2]]])), 5, 35)
-        oled.show()
-        return
+        while not button_a.is_pressed():
+            oled.fill(0)
+            oled.DispChar_font(SeniorOS.fonts.misans, DLTIME, 5, 10)
+            oled.DispChar_font(SeniorOS.fonts.misans_16, (''.join([str(x) for x in [time.localtime()[1], '/', time.localtime()[2]]])), 5, 35)
+            oled.show()
+            if eval("[/GetButtonExpr('py')/]"):
+                PagesManager.Main.Import("SeniorOS.system.pages", "ShutDown")
 
     def Page1():
         while not eval("[/GetButtonExpr('py')/]") or eval("[/GetButtonExpr('on')/]"):
             oled.fill(0)
             myUI = UI(oled)
-            oled.DispChar(str('运行时间'), 5, 0, 1)
-            oled.DispChar(str(time.time()) + "s", 5, 16, 1)
-            oled.DispChar(str('今日已过'), 5, 32, 1)
+            oled.DispChar('运行时间', 5, 0, 1)
+            oled.DispChar("{}s".format(time.time()), 5, 16, 1)
+            oled.DispChar('今日已过', 5, 32, 1)
             myUI.ProgressBar(5, 50, 50, 8, time.localtime()[3] // 0.24)
             myUI.qr_code('https://senior.flowecho.org', 65, 0, scale=2)
             oled.show()
+            if eval("[/GetButtonExpr('th')/]"):
+                PagesManager.Main.Import("SeniorOS.system.pages", "ShutDown")
 
 
 Style3 = Page0 = Style3Dependencies.Page0
